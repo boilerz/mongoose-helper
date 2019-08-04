d64057652e6aaf9e6891c3f8aca9f8215a489a0c
momsse <momssesystem@gmail.com>
2019-08-04 18:55:16 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# @boilerz/mongoose-helper
+
+[![GitHub package.json version](https://img.shields.io/github/package-json/v/boilerz/mongoose-helper)](https://www.npmjs.com/package/@boilerz/mongoose-helper)
+
+> Mongoose helper.
+
+### Install
+
+```bash
+npx install-peerdeps @boilerz/mongoose-helper
+```
+
+### Usage
+
+| Name                  | Default                                 | Description                                    |
+|-----------------------|-----------------------------------------|------------------------------------------------|
+| MONGO_DB_URI          | `http://localhost:27017/REPO_DIRECTORY` | Mongo DB uri                                   |
+| MONGO_DB_AUTO_CONNECT |                                         | Will auto connect to the database when `true`. |
+
+### Release
+
+```bash
+yarn version
+yarn build
+yarn publish dist --access public
+```
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+// / <reference types="mongodb" />
+
+import path from 'path';
+import process from 'process';
+import mongoose, { ConnectionOptions, Mongoose } from 'mongoose';
+import logger from '@boilerz/logger';
+
+const autoConnect: boolean = process.env.MONGO_DB_AUTO_CONNECT === 'true';
+const defaultUri: string =
+  process.env.MONGO_DB_URI ||
+  `http://localhost:27017/${path.basename(process.cwd())}`;
+
+export function instance(): Mongoose {
+  return mongoose;
+}
+
+export async function connect(
+  uri?: string,
+  options?: ConnectionOptions,
+): Promise<Mongoose | void> {
+  const mongooseInstance = await mongoose.connect(uri || defaultUri, options);
+  logger.info('[mongo-helper] ✓ Connection successful');
+  return mongooseInstance;
+}
+
+export function disconnect(): Promise<void> {
+  logger.info('[mongo-helper] ✓ Disconnection successful');
+  return mongoose.disconnect();
+}
+
+export function dropDatabase(): Promise<void> {
+  logger.info('[mongo-helper] ⚠️ Dropping database');
+  return mongoose.connection.dropDatabase();
+}
+
+if (autoConnect) {
+  logger.info('[mongoose-helper] Auto connecting');
+
+  connect().catch((err: Error): void =>
+    logger.error({ err }, '[mongoose-helper] Auto connect failure'),
+  );
+}
